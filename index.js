d3c15b8e460d18729107f3603fd087ae87d8372b
Qlex-cmos <alexcheula@gmail.com>
2024-09-28 23:24:42 +0200

    differentes variables de JS

---
+++ index.js
@@ -0,0 +1,12 @@
+// alert("salut les foufous")
+
+let variable = "hello";
+console.log(variable);
+
+//**** VARIABLES ****//*
+
+//  const -> (constante) ce type de variable ne peut plus être modifié après affectation
+//  var -> vieux Javascript
+//  let -> variable qui peut évoluer en fonction des besoins
+
+//38:04
